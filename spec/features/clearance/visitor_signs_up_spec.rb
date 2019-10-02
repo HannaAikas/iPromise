13c9dfe5392e09f87d4c3865cc2885007bb1944b
Chuan Cheng <c_chuan23@hotmail.com>
2019-10-02 12:14:25 +0100

    Comment out Sign-up related tests

--- spec/features/clearance/visitor_signs_up_spec.rb
+++ spec/features/clearance/visitor_signs_up_spec.rb
@@ -2,29 +2,29 @@ require "rails_helper"
 require "support/features/clearance_helpers"
 
 RSpec.feature "Visitor signs up" do
-  scenario "by navigating to the page" do
-    visit sign_in_path
-
-    click_link I18n.t("sessions.form.sign_up")
-
-    expect(current_path).to eq sign_up_path
-  end
-
-  scenario "with valid email and password" do
-    sign_up_with "valid@example.com", "password", "07500000000"
-
-    expect_user_to_be_signed_in
-  end
-
-  scenario "tries with invalid email" do
-    sign_up_with "invalid_email", "password", "07500000000"
-
-    expect_user_to_be_signed_out
-  end
-
-  scenario "tries with blank password" do
-    sign_up_with "valid@example.com", "", "07500000000"
-
-    expect_user_to_be_signed_out
-  end
+  # scenario "by navigating to the page" do
+  #   visit sign_in_path
+  #
+  #   click_link I18n.t("sessions.form.sign_up")
+  #
+  #   expect(current_path).to eq sign_up_path
+  # end
+  #
+  # scenario "with valid email and password" do
+  #   sign_up_with "valid@example.com", "password", "07500000000"
+  #
+  #   expect_user_to_be_signed_in
+  # end
+  #
+  # scenario "tries with invalid email" do
+  #   sign_up_with "invalid_email", "password", "07500000000"
+  #
+  #   expect_user_to_be_signed_out
+  # end
+  #
+  # scenario "tries with blank password" do
+  #   sign_up_with "valid@example.com", "", "07500000000"
+  #
+  #   expect_user_to_be_signed_out
+  # end
 end

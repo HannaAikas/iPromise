814a0791ad8447ca6f959bfeac1595d71c64711d
Chuan Cheng <c_chuan23@hotmail.com>
2019-10-02 22:53:29 +0100

    Fix issue of not able to store new user in 'user' database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   include Clearance::User
 
-  include Clearance::User  
+  include Clearance::User
   validates :mobile, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end

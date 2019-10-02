0495a036e18858c4025c4a91158cec932b510507
monmon8 <imaneben8@gmail.com>
2019-10-02 11:50:04 +0100

    fixing rename column

--- db/migrate/20191002091321_rename_mobile_number_to_mobile_number.rb
+++ db/migrate/20191002091321_rename_mobile_number_to_mobile_number.rb
@@ -1,5 +1,5 @@
 class RenameMobileNumberToMobileNumber < ActiveRecord::Migration[6.0]
   def change
-    rename_column :users, :mobile, :mobile
+    rename_column :users, :mobileNumber, :mobile
   end
 end

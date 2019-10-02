ab2256850e3a98a9d74c25d3b8f29e20a9ac9349
monmon8 <imaneben8@gmail.com>
2019-10-02 10:23:45 +0100

    adding column with the correct name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   include Clearance::User
 
-  include Clearance::User
-  
-  validates :mobileNumber, presence: true, uniqueness: true
-
+  include Clearance::User  
+  validates :mobile, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_142645) do
+ActiveRecord::Schema.define(version: 2019_10_02_091321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(version: 2019_10_01_142645) do
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
-    t.integer "mobileNumber"
+    t.integer "mobile"
     t.index ["email"], name: "index_users_on_email"
-    t.index ["mobileNumber"], name: "index_users_on_mobileNumber"
+    t.index ["mobile"], name: "index_users_on_mobile"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 

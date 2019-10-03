9919a5aee66b9536c3ab5a7d65a2f3e4e7532faf
harris_lau@hotmail.co.uk <harris_lau@hotmail.co.uk>
2019-10-03 11:31:31 +0100

    Mobile number now stores in database as a string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_091321) do
+ActiveRecord::Schema.define(version: 2019_10_01_150959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2019_10_02_091321) do
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
-    t.integer "mobile"
+    t.string "mobile"
     t.index ["email"], name: "index_users_on_email"
     t.index ["mobile"], name: "index_users_on_mobile"
     t.index ["remember_token"], name: "index_users_on_remember_token"

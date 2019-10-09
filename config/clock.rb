68de789548c17bfbd7005deb261e9305a93159a0
Chuan Cheng <c_chuan23@hotmail.com>
2019-10-09 13:04:20 +0100

    use web app instead of background worker to send message

--- config/clock.rb
+++ config/clock.rb
@@ -2,14 +2,20 @@ require 'clockwork'
 require_relative './config/boot'
 require_relative './config/environment'
 require_relative '../app/workers/worker.rb'
+require_relative '../app/background_jobs/reminder.rb'
 
 module Clockwork
   handler do |job|
     puts "Running #{job}"
   end
 
-  every(2.minutes, 'Check and send out messages', tz: 'UTC'){
-    OurWorker.perform_async
+  # every(2.minutes, 'Check and send out messages', tz: 'UTC'){
+  #   OurWorker.perform_async
+  # }
+
+  every(2.minutes, 'Check and send out message', tz: 'UTC'){
+    new_send_msg = Reminder.new
+    new_send_msg.iterate_database
   }
 
 end

f385036be70d7c5fb0c73fb89cde436c86c79542
Chuan Cheng <c_chuan23@hotmail.com>
2019-10-11 15:51:39 +0100

    refactor reminder.rb excluding test

--- app/background_jobs/reminder.rb
+++ app/background_jobs/reminder.rb
@@ -2,64 +2,51 @@ require 'date'
 require_relative './text_sender'
 
 class Reminder
+  DEFAULT_FREQ = 120
 
-  def initialize(text_sender=TextSender.new)
+  def initialize(text_sender=TextSender.new, freq=DEFAULT_FREQ)
     @text_sender = text_sender
+    @freq = freq
+    @current_promise = nil
   end
 
   def iterate_database
-    puts "Checking promise database..."
     Promise.all.each do |promise|
-      process(promise)
+      @current_promise = promise
+      process()
     end
-    puts "Iterate done!!"
   end
 
-  def process(promise)
+  def send_final_message(mobile, name)
+      @text_sender.send_text(mobile, "Hey there #{name}! Have you kept your promise?
+        If YES, click: cryptic-thicket-87200.herokuapp.com/promises/#{@current_promise.id}/congrats
+        or If NO, click: cryptic-thicket-87200.herokuapp.com/promises/#{@current_promise.id}/punishment")
+      @current_promise.status = 'false'
+      @current_promise.save
+  end
 
-    puts "DEBUG STATUS: #{promise.status}"
-    return if promise.status == false
+  def send_reminder(mobile, name)
+    promise_content = promise.text
+    promise_content = promise.other_text if promise_content == 'Other'
+    @text_sender.send_text(mobile, "Here is your promise: #{promise_content}")
+    @current_promise.last_reminder_time = Time.now.utc
+    @current_promise.save
+  end
 
-    current_time = Time.now.utc
-    one_day_timedifference = 60*2
+  def process
+    return if @current_promise.status == false
 
-    user_id = promise.user_id
-    puts "DEBUG: USERID #{user_id}"
-    user_info = User.find(user_id)
+    user_info = User.find(@current_promise.user_id)
     user_mobile = user_info.mobile
     user_name = user_info.firstname
 
-    puts "current_time: #{current_time}"
-    puts "last_time: #{promise.last_reminder_time.to_time}"
-    puts "diff: #{current_time - promise.last_reminder_time.to_time}"
-    puts "end_time: #{promise.end_datetime.to_time}"
-
-    # check and send out final message
-    if promise.end_datetime.to_time <= current_time
-      puts "Sending final text and close promise..."
-      @text_sender.send_text(user_mobile, "Hey there #{user_name}! Have you kept your promise?
-        If YES, click: cryptic-thicket-87200.herokuapp.com/promises/#{promise.id}/congrats
-        or If NO, click: cryptic-thicket-87200.herokuapp.com/promises/#{promise.id}/punishment")
-      puts "final message sent"
-      promise.status = 'false'
-      promise.save
-      puts "promise closed"
+    if @current_promise.end_datetime.to_time <= Time.now.utc
+      send_final_message(user_mobile, user_name)
       return
     end
 
-    # check and send out reminder
-    if current_time - promise.last_reminder_time.to_time > one_day_timedifference
-      puts "Sending reminder..."
-      promise_content = promise.text
-      promise_content = promise.other_text if promise_content == 'Other'
-
-      @text_sender.send_text(user_mobile, "Here is your promise: #{promise_content}")
-      promise.last_reminder_time = current_time
-      promise.save
-      puts "reminder sent"
+    if current_time - promise.last_reminder_time.to_time > @freq
+      send_reminder(user_mobile, user_name)
     end
-
-    puts "Finishing processing current promise..."
   end
-
 end
